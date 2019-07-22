5431c8d74962af1122619ce0ffdbdbecb038613f
wojiaolds <1198986674@qq.com>
2019-07-22 18:48:44 +0800

    test

--- Dynamic-Injection/src/main/java/com/lds/dyn/importBeanDefinitionRegistrar/CountryMapper.java
+++ Dynamic-Injection/src/main/java/com/lds/dyn/importBeanDefinitionRegistrar/CountryMapper.java
@@ -1,10 +1,22 @@
 package com.lds.dyn.importBeanDefinitionRegistrar;
 
 import com.lds.dyn.importBeanDefinitionRegistrar.annotation.MyMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * @Author: lds
  * @Date: 2019/7/16 9:29
  */
 @MyMapper
-public class CountryMapper { }
+public class CountryMapper {
+	
+	@Autowired
+	private StuMapper stuMapper;
+	
+	public void print(){
+		stuMapper.print ();
+	}
+	
+	
+}
