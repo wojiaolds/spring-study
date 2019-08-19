a3083d700def6875442c2245afad4a9edd169ac2
wojiaolds <1198986674@qq.com>
2019-08-19 17:37:36 +0800

    test

--- java-study/src/main/java/test/TwoBigNumberAdd.java
+++ java-study/src/main/java/test/TwoBigNumberAdd.java
@@ -11,9 +11,43 @@ public class TwoBigNumberAdd {
 		String s2 = "324254354654";
 		add(s1,s2);
 	}
-	public static String add(String s1,String s2){
+	public static String add(String str1,String str2){
+		if(str1 == null)
+			return str2;
+		if(str2 == null)
+			return str1;
+		StringBuffer s1 = new StringBuffer(str1).reverse();
+		StringBuffer s2 = new StringBuffer(str2).reverse();
+		StringBuffer res = new StringBuffer();
+		int len1 = s1.length();
+		int len2 = s2.length();
+		int len;
+		if(len1 < len2) {
+			len = len2;
+			int count = len2 - len1;
+			while(count-- > 0)
+				s1.append('0');
+		} else {
+			len = len1;
+			int count = len1 - len2;
+			while(count-- > 0)
+				s2.append('0');
+		}
+		int overflow = 0;
+		int num;
+		for(int i = 0; i < len; i++) {
+			num = s1.charAt(i) - '0' + s2.charAt(i) - '0' + overflow;
+			if(num >= 10) {
+				overflow = 1;
+				num -= 10;
+			} else {
+				overflow = 0;
+			}
+			res.append(String.valueOf(num));
+		}
+		if(overflow == 1)
+			res.append(1);
 		
-		
-		return null;
+		return res.reverse().toString();
 	}
 }
